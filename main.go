8d9b7de9733c5adeb45e234af73ec42ba61487c9
JohnnyStein <deeptechnologiescompany@gmail.com>
2020-10-31 18:35:34 -0300

    Update

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+// docker run <container> cmd args
+// go run main.go cmd args
 
 func main() {
-	fmt.Println("Hello, World!")
+	switch os.Args[1] {
+	case "run":
+		run()
+	default:
+		panic("what??")
+	}
+
+}
+
+func run() {
+	fmt.Printf("running %v\n", os.Args[2:])
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
+	}
+
+	must(cmd.Run())
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
