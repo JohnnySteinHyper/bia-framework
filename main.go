56e8eac71f5cb0edd902ac2fc4ea1c3c775454ff
JohnnyStein <deeptechnologiescompany@gmail.com>
2020-10-31 19:17:48 -0300

    Update

--- main.go
+++ main.go
@@ -19,3 +19,41 @@ func info() {
   app.Author = "JohnnyStein" 
   app.Version = "1.0.0"
 }
+
+func commands() {
+  app.Commands = []cli.Command{
+    {
+      Name:    "peppers",
+      Aliases: []string{"p"},
+      Usage:   "Add peppers to your pizza",
+      Action: func(c *cli.Context) { 
+        pe := "peppers"
+        peppers := append(pizza, pe)
+        m := strings.Join(peppers, " ")
+        fmt.Println(m)
+      },
+    },
+    {
+      Name:    "pineapple",
+      Aliases: []string{"pa"},
+      Usage:   "Add pineapple to your pizza",
+      Action: func(c *cli.Context) { 
+        pa := "pineapple"
+        pineapple := append(pizza, pa)
+        m := strings.Join(pineapple, " ")
+        fmt.Println(m)
+      },
+    },
+    {
+      Name:    "cheese",
+      Aliases: []string{"c"},
+      Usage:   "Add cheese to your pizza",
+      Action: func(c *cli.Context) { 
+        ch := "cheese"
+        cheese := append(pizza, ch)
+        m := strings.Join(cheese, " ")
+        fmt.Println(m)
+      },
+    },
+  }
+}
